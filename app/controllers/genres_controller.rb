57111833abdb1b699a31782662dd47b335d058c5
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-09-13 02:13:06 +0100

    edited database records by adding edit

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,37 @@ class GenresController < ApplicationController
   def index
     @genres = Genre.all
   end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(params.require(:genre).permit(:genre_type, :body))
+
+    respond_to do |format|
+      if @genre.save
+        format.html { redirect_to genres_url(@genres), notice: "Genre was successfully created." }
+
+      else
+        format.html { render :new, status: :unprocessable_entity }
+
+      end
+    end
+  end
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  def update
+    @genre = Genre.find(params[:id])
+    respond_to do |format|
+      if @genre.update(params.require(:genre).permit(:genre_type, :body))
+        format.html { redirect_to genres_url(@genres), notice: "Genre was successfully updated." }
+
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+
+      end
+    end
+  end
 end
