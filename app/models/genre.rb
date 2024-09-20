cd8c617e0a5b0ad27e8d79088f43ec9a960c7780
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-09-20 21:46:30 +0100

    custom scope creation

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
-  validates_presence_of :genre_type, :body
+  validates_presence_of :genre_type, :body, :main_image, :thumb_image
+
+  scope :senin, -> { where(genre_type: "Shonen #{3}") }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
 10.times do |genre|
-  Genre.create!(genre_type:  "genre type #{genre}",
+  Genre.create!(genre_type:  "Senin #{genre}",
                body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
                main_image: "http://via.placeholder.com/600x400",
                thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+10.times do |genre|
+  Genre.create!(genre_type:  "Shonen #{genre}",
+                body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                main_image: "http://via.placeholder.com/600x400",
+                thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
   )
 end
 
@@ -15,9 +30,10 @@ end
   )
 end
 
-10.times do |blogs|
+8.times do |blogs|
   Blog.create!(
   title: "blog Title #{blogs}",
   body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+  topic_id: Topic.last.id
   )
 end
