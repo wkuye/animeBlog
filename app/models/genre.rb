5027a0957cb3a7430084acd234b579259e244b89
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-09-21 16:15:54 +0100

    using concerns for our Genre model, which serve as helpers to our code and mainly like a code splitter

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,11 +1,12 @@
 class Genre < ApplicationRecord
+  includes Placeholder
   validates_presence_of :genre_type, :body, :main_image, :thumb_image
 
   scope :senin, -> { where(genre_type: "Shonen #{3}") }
 
   after_initialize :set_defaults
   def set_defaults
-    self.main_image  ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/300x200"
+    self.main_image  ||= Placeholder.image_generator(300,300)
+    self.thumb_image ||= Placeholder.image_generator(200,250)
   end
 end
