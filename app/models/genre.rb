7edb23bdfb940599f3e0686cbb9ff246d33d99b6
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-09-21 15:43:39 +0100

    implmented image defaults for genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,10 @@ class Genre < ApplicationRecord
   validates_presence_of :genre_type, :body, :main_image, :thumb_image
 
   scope :senin, -> { where(genre_type: "Shonen #{3}") }
+
+  after_initialize :set_defaults
+  def set_defaults
+    self.main_image  ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/300x200"
+  end
 end
