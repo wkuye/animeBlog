fd693f589eb3a8782294443ff167fbccdfd9560d
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-12-02 18:23:56 +0100

    refactored application controller to include concern for our source feature

---
+++ app/controllers/concerns/set_source.rb
@@ -0,0 +1,10 @@
+module SetSource
+  extend ActiveSupport::Concern
+included do
+  before_action :sessions_function
+end
+
+  def sessions_function
+    session[:source]= params[:q] if params[:q]
+  end
+end
