b3bf22d4e68fb070225a1ca897fdfc2409392c07
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-12-02 18:12:40 +0100

    added sessions to our application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include DeviseWhitelist
-  
+  before_action :sessions_function
+#created a session and passsed a query in our params if it's true
+  def sessions_function
+    session[:source]= params[:q] if params[:q]
+  end
 end
