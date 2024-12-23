41f0d1ee9004352bd44e1f777159e5c71962a500
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2024-12-23 19:05:02 +0100

    implemented viewhelpers for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+     link_to "Logout", destroy_user_session_path, method: :delete
+    else
+    #this combine the register and the login together and the br moves the login to the next line 
+    ( link_to "Register", new_user_registration_path) +
+ "<br>".html_safe +
+     (link_to "Login", new_user_session_path)
+    end 
+  end
 end
