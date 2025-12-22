8f8ea697c85af460c29f6b6ba085d449b686c055
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2025-12-23 00:56:44 +0100

    Fix bootstrap-icons require for Heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require "bootstrap-icons"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
