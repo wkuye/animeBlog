25f336cf3195fb447ba25d820535bacb704e2cec
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2025-12-23 01:33:50 +0100

    Remove add_topic_references_to_blogs migration

--- db/migrate/20240920171813_remove_foreign_key_and_topics_id_from_blogs.rb
+++
@@ -1,9 +0,0 @@
-class RemoveForeignKeyAndTopicsIdFromBlogs < ActiveRecord::Migration[7.2]
-  def change
-    # Remove the foreign key constraint first
-    remove_foreign_key :blogs, column: :topics_id
-
-    # Remove the topics_id column
-    remove_column :blogs, :topics_id, :integer
-  end
-end
