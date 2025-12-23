8e9e449b56bf23fd229a731cde5977c5be44c833
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2025-12-23 01:55:11 +0100

    fix remove animeID from news table in database migration

--- db/migrate/20251203095605_remove_anime_id_from_news.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAnimeIdFromNews < ActiveRecord::Migration[7.2]
-  def change
-    if foreign_key_exists?(:news, :animes)
-      remove_reference :news, :anime, foreign_key: true
-    else
-      remove_reference :news, :anime
-    end
-  end
-end
