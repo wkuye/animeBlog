1d97748edb5307155153681a18f282be3703df84
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2025-12-23 01:30:20 +0100

    Remove add_topic_references_to_blogs migration

--- db/migrate/20240920165232_add_topic_refrences_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddTopicRefrencesToBlogs < ActiveRecord::Migration[7.2]
-  def change
-    add_reference :blogs, :topic,  foreign_key: true
-  end
-end
--- db/migrate/20250522182921_remove_topic_id_from_blogs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTopicIdFromBlogs < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :blogs, :topic_id
-  end
-end
