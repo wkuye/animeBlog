b974f6c9dc4a08f7700c60d2e145149238fba49c
OLAWALE KUYE <olawalekuye@OLAWALEs-MacBook-Pro.local>
2025-12-31 22:14:08 +0100

    fixed all issues

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -11,6 +11,7 @@ class Anime < ApplicationRecord
   includes Placeholder
   validates_presence_of :title, :description, :main_image, :thumb_image,  :episodes, :thumb_video_url
   after_initialize :set_defaults
+
   def set_defaults
     self.main_image  ||= Placeholder.image_generator(300, 300)
     self.thumb_image ||= Placeholder.image_generator(200, 250)
